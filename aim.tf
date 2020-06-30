14e0f3d5fec3c79904e2a1092d9be9a2c3b8b588
Dima Kuzmenko <super-zomby@ukr.net>
2020-06-30 13:32:00 +0000

    del all

--- aim.tf
+++
@@ -1,5 +0,0 @@
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
-  key_name = var.key_name
-  public_key = file("~/.ssh/${var.key_name}.pub")
-}
--- output.tf
+++
@@ -1,5 +0,0 @@
-output "webserver_public_ip_address" {
-   value = aws_eip.my_static_ip.public_ip
-}
-
-
